9684951ead7523cdd1974fd93d4a0f0f12badc18
bryang10 <56742984+bryang10@users.noreply.github.com>
2019-10-18 21:47:53 -0500

    Add files via upload

---
+++ ejercicio9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 15; i++)
+            {
+                Console.WriteLine("\nTabla de multiplicar del {0}", i);
+                Console.WriteLine("------------------------------");
+                for (int j = 1; j <= 12; j++)
+                {
+                    Console.WriteLine("{0} * {1} = {2}", i, j, (i * j));
+                }
+            }
+        }
+    }
+}
