431c39471b1f30a2ad47c73b59113bc5d11f1aad
bryang10 <56742984+bryang10@users.noreply.github.com>
2019-10-18 21:42:12 -0500

    Add files via upload

---
+++ ejercicio4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cant;
+            double precio = 0; 
+            double total=0;
+
+            do
+            {
+                Console.WriteLine("Introdusca cantidad vendida en entero positivo:");
+                cant = int.Parse(Console.ReadLine());
+                if (cant != 0)
+                {
+                    Console.WriteLine("Introdusca precio :");
+                    cant = int.Parse(Console.ReadLine());
+                }
+                Console.WriteLine("es igual a:" + total = total + (cant * precio));
+
+            } while (cantidad != 0);
+            Console.WriteLine("el total es"+total);
+            Console.ReadKey();
+        }
+    }
+}
