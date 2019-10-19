7cabd8262bf6e6d11140449760900de266859735
bryang10 <56742984+bryang10@users.noreply.github.com>
2019-10-18 21:46:45 -0500

    Add files via upload

---
+++ ejercicio8/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ejercicio8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese numero entero");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            int doble = 0;
+            int triple = 0;
+
+            for(int a = 1; a <=numero; a++)
+            {
+                Console.Write(a);
+                Console.Write(" ");
+                
+                doble = doble + 2;
+
+                if(doble>numero)
+                {
+                    Console.Write(" ");
+
+                }
+                else
+                {
+                    Console.Write(doble);
+
+                }
+                Console.Write(" ");
+                triple = triple + 3;
+                if(triple>numero)
+                {
+                    Console.Write(" ");
+
+                }
+                else
+                {
+                    Console.Write(triple);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
