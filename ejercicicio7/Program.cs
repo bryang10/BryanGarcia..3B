4ef5b2a354dae984c2615833acf583c6730cd009
bryang10 <56742984+bryang10@users.noreply.github.com>
2019-10-18 21:45:34 -0500

    Add files via upload

---
+++ ejercicicio7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ejercicicio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 1; x <= 100; x++)
+            {
+                Console.WriteLine(x + "\t");
+                Console.ReadLine();
+             
+            }
+            for (int x = 100; x >=1; x++)
+            {
+                Console.WriteLine(x + "\t");
+                Console.ReadLine();
+              
+            }
+            Console.ReadKey();
+        }
+    }
+}
